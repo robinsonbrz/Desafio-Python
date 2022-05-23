2efe683c56219d799691f9c1fc7de123d09e1093
ROBINSON ENEDINO E SILVA <robinsonbrz@gmail.com>
2022-05-23 16:06:24 -0300

    rascunho classes

---
+++ Partida.py
@@ -0,0 +1,38 @@
+
+
+
+
+class Jogador:
+    def __init__(self, saldo, numero_vitorias):
+        self.saldo = 300
+        self.numero_vitorias = 0
+
+class Jogador_impulsivo(Jogador):
+    def compra_imovel():
+        # verificar se o saldo é suficiente 
+        return True
+
+
+class Jogador_exigente(Jogador):
+    def compra_imovel():
+        # verificar se o saldo é suficiente 
+        # compra qualquer propriedade com valor de aluguel maior do que 50
+        # se compra subtrai valor movel do saldo
+        return 
+
+
+class Jogador_cauteloso(Jogador):
+    def compra_imovel():
+        # compra qualquer imovel mas deve lhe sobrar 80 de saldo na transação
+
+
+        return
+
+
+class Jogador_aleatrio(Jogador):
+    def compra_imovel():
+        # verificar se o saldo é suficiente 
+        
+        # Comprador aleatório, com 50% de chance de comprar
+
+

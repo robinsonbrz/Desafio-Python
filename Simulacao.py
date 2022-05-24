1503d6d6568ce687f4b290637c34370d002e3646
ROBINSON ENEDINO E SILVA <robinsonbrz@gmail.com>
2022-05-24 05:18:42 -0300

    primeiros testes

--- Simulacao.py
+++ Simulacao.py
@@ -4,10 +4,6 @@ class Simulacao:
         self.encerradas_timeout = 0
         self.rodadas_partida = []
         self.vitoriosos = []
-        
-
-
-
 
 
     def __str__(self):
@@ -15,20 +11,33 @@ class Simulacao:
         # média de roddadas das partidas
         # percentagem de vitórias por comportamento de jogadores
         # comportamento que mais venceu
-
         comportamentos = set(self.vitoriosos)
 
-        status_comportamentos = {}
+        nome = []
+        valor = []
+        percentual = []
+        maior = 0
+
+        media = round(self.media_rodadas(),2)
+        saida = f"\n\n\nPartidas finalizadas por timeout: {self.encerradas_timeout}\nMédia de rodadas para o fim de partida: {media}\n\n"
+
+
         for comportamento in comportamentos:
-            status_comportamentos[comportamento] = self.vitoriosos.count(comportamento), round((self.vitoriosos.count(comportamento)/300)*100,2)
+            nome.append(comportamento)
+            valor.append(self.vitoriosos.count(comportamento))
+            percent = round((self.vitoriosos.count(comportamento)/300)*100,2)
+            percentual.append(percent)
+            saida += f"Quantidade de vitórias de {comportamento}: {self.vitoriosos.count(comportamento)} que representou {percent}%  de vitórias\n"
+            saida += f"Percentual de vitórias de {comportamento}: {percent}\n\n"
+            
+        maior = max(valor)
+        index = valor.index(maior)
+        nome_maior = nome[index]
 
-
-        media = self.media_rodadas()
-        saida = f"Partidas finalizadas por timeout: {self.encerradas_timeout}\n Média de rodadas: {media} \n   {status_comportamentos}   "
-
+        saida += f"Comportamento com maior vitórias: {nome_maior}\n_________________________________________________________"
         return saida
 
-    def media_rodadas(self):
 
+    def media_rodadas(self):
         return sum(self.rodadas_partida)/len(self.rodadas_partida)
 
